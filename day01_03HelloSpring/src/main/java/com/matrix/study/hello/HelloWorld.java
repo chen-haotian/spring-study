4017bfd17a464687c228cc7694e4a195ac3517b9
matrix <99764360@qq.com>
2022-05-04 21:15:38 +0800

    [+]Spring入门案例HelloWorld

---
+++ day01_03HelloSpring/src/main/java/com/matrix/study/hello/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.matrix.study.hello;
+
+/**
+ * @Author: Matrix
+ * @CreateDate: 2022-05-04 12:01:59
+ * @Description: TODO Spring入门案例
+ * @Version: TODO
+ */
+public class HelloWorld {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}

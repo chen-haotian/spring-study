1d60e30f22de1dbb82d483af39f4e94b0d3e4661
matrix <99764360@qq.com>
2022-05-07 15:24:54 +0800

    [+]SpringAOP的基于XML文件的配置

---
+++ day03_01SpringAOP/src/main/java/com/matrix/study/service/impl/AccountServiceImpl.java
@@ -0,0 +1,28 @@
+package com.matrix.study.service.impl;
+
+import com.matrix.study.service.IAccountService;
+
+/**
+ * @Author: Matrix
+ * @CreateDate: 2022-05-07 12:45:22
+ * @Description: TODO
+ * @Version: TODO
+ */
+public class AccountServiceImpl implements IAccountService {
+
+    @Override
+    public void saveAccount() {
+        System.out.println("执行了保存操作");
+    }
+
+    @Override
+    public void updateAccount(int i) {
+        System.out.println("执行了更新操作" + i);
+    }
+
+    @Override
+    public int deleteAccount() {
+        System.out.println("执行了删除操作");
+        return 0;
+    }
+}
